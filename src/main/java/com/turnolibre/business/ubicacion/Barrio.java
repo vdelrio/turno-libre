3a6a834a86ba165028905447d3d04bef7cc67dc6
toto84toto <toto84toto@ca7791b7-9e57-4a50-a5bf-1408515eedb4>
2013-11-18 23:51:27 +0000

    Se enriquecen las clases Barrio y Ubicación

--- src/main/java/com/turnolibre/business/ubicacion/Barrio.java
+++ src/main/java/com/turnolibre/business/ubicacion/Barrio.java
@@ -1,7 +1,7 @@
 package com.turnolibre.business.ubicacion;
 
 /**
- *
+ *  Representa al barrio en el que se encuentra un prestador de servicios.
  *
  * @author Victor Del Rio
  */
@@ -11,5 +11,75 @@ public class Barrio {
 	private Ciudad ciudad;
 
 	private String nombre;
+
+
+	/*------------------------------------ Constructors ------------------------------------*/
+
+	protected Barrio() {
+		super();
+	}
+
+	public Barrio(String nombre, Ciudad ciudad) {
+
+		this.nombre = nombre;
+		this.ciudad = ciudad;
+	}
+
+	/*--------------------------------------------------------------------------------------*/
+    /*---------------------------------- Geters and seters ---------------------------------*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/*--------------------------------------------------------------------------------------*/
+    /*-------------------------------- Hash code and equals --------------------------------*/
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (this == o) return true;
+		if (o == null || !(o instanceof Barrio)) return false;
+
+		Barrio otroBarrio = (Barrio) o;
+
+		if (!this.getCiudad().equals(otroBarrio.getCiudad())) return false;
+		if (!this.getNombre().equals(otroBarrio.getNombre())) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = ciudad.hashCode();
+		result = 31 * result + nombre.hashCode();
+		return result;
+	}
+
+	/*--------------------------------------------------------------------------------------*/
+	/*-------------------------------------- Overrides -------------------------------------*/
+
+	@Override
+	public String toString() {
+		return this.getNombre();
+	}
+
+    /*--------------------------------------------------------------------------------------*/
 
 }
--- src/main/java/com/turnolibre/business/ubicacion/Ubicacion.java
+++ src/main/java/com/turnolibre/business/ubicacion/Ubicacion.java
@@ -8,10 +8,82 @@ package com.turnolibre.business.ubicacion;
 public class Ubicacion {
 
 	private Ciudad ciudad;
-
-	// Nullable
 	private Barrio barrio;
-
 	private String direccion;
+
+
+	/*------------------------------------ Constructors ------------------------------------*/
+
+	protected Ubicacion() {
+		super();
+	}
+
+	public Ubicacion(Ciudad ciudad, Barrio barrio, String direccion) {
+
+		this.ciudad = ciudad;
+		this.barrio = barrio;
+		this.direccion = direccion;
+	}
+
+	public Ubicacion(Ciudad ciudad, String direccion) {
+
+		this.ciudad = ciudad;
+		this.direccion = direccion;
+	}
+
+	/*--------------------------------------------------------------------------------------*/
+    /*---------------------------------- Geters and seters ---------------------------------*/
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public Barrio getBarrio() {
+		return barrio;
+	}
+
+	public void setBarrio(Barrio barrio) {
+		this.barrio = barrio;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	/*--------------------------------------------------------------------------------------*/
+    /*-------------------------------- Hash code and equals --------------------------------*/
+
+	@Override
+	public boolean equals(Object o) {
+
+		if (this == o) return true;
+		if (o == null || !(o instanceof Ubicacion)) return false;
+
+		Ubicacion otraUbicacion = (Ubicacion) o;
+
+		if (this.getBarrio() != null ? !this.getBarrio().equals(otraUbicacion.getBarrio()) : otraUbicacion.getBarrio() != null) return false;
+		if (!this.getCiudad().equals(otraUbicacion.getCiudad())) return false;
+		if (!this.getDireccion().equals(otraUbicacion.getDireccion())) return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = this.getCiudad().hashCode();
+		result = 31 * result + (this.getBarrio() != null ? this.getBarrio().hashCode() : 0);
+		result = 31 * result + this.getDireccion().hashCode();
+		return result;
+	}
+
+	/*--------------------------------------------------------------------------------------*/
 
 }
