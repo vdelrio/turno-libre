86799375299fcd2bc2185941027d8a2e00579af3
toto84toto <toto84toto@ca7791b7-9e57-4a50-a5bf-1408515eedb4>
2013-11-30 21:05:27 +0000

    Ciudad en el cliente

--- trunk/src/main/java/com/turnolibre/business/usuario/Cliente.java
+++ trunk/src/main/java/com/turnolibre/business/usuario/Cliente.java
@@ -1,6 +1,7 @@
 package com.turnolibre.business.usuario;
 
 import com.turnolibre.business.turno.Turno;
+import com.turnolibre.business.ubicacion.Ciudad;
 
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -14,16 +15,45 @@ public class Cliente extends Rol {
 
 	public static String NOMBRE_DE_ROL = "cliente";
 	
-//	private Ciudad ciudad;
+	private Ciudad ciudad;
 	private SortedSet<Turno> turnos = new TreeSet<Turno>();
+
+
+	/*------------------------------------ Constructors ------------------------------------*/
+
+	// TODO pasar a protected
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Ciudad ciudad) {
+		super();
+		this.ciudad = ciudad;
+	}
+
+    /*--------------------------------------------------------------------------------------*/
+	/*------------------------------------ Public methods ----------------------------------*/
 
 	@Override
 	public String getNombreDeRol() {
 		return NOMBRE_DE_ROL;
 	}
 
+    /*--------------------------------------------------------------------------------------*/
+	/*---------------------------------- Geters and seters ---------------------------------*/
+
+	public Ciudad getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(Ciudad ciudad) {
+		this.ciudad = ciudad;
+	}
+
 	public SortedSet<Turno> getTurnos() {
 		return turnos;
 	}
+
+	/*--------------------------------------------------------------------------------------*/
 
 }
--- trunk/src/test/java/util/PopuladorDeDatosDeTest.java
+++ trunk/src/test/java/util/PopuladorDeDatosDeTest.java
@@ -1,45 +1,31 @@
 package util;
 
 import com.turnolibre.business.agenda.Agenda;
-import com.turnolibre.business.agenda.DiaNoLaboral;
-import com.turnolibre.business.agenda.JornadaLaboralHabitual;
-import com.turnolibre.business.agenda.JornadaLaboralOcasional;
 import com.turnolibre.business.excepcion.ExcepcionDeReglaDelNegocio;
-import com.turnolibre.business.joda.time.DayOfWeekTime;
 import com.turnolibre.business.prestador.PrestadorDeServicios;
 import com.turnolibre.business.prestador.Rubro;
-import com.turnolibre.business.prestador.Servicio;
-import com.turnolibre.business.turno.Horario;
 import com.turnolibre.business.ubicacion.Barrio;
 import com.turnolibre.business.ubicacion.Ciudad;
 import com.turnolibre.business.ubicacion.Ubicacion;
-import com.turnolibre.business.usuario.AdministradorDeAgenda;
 import com.turnolibre.business.usuario.Cliente;
 import com.turnolibre.business.usuario.Usuario;
-import com.turnolibre.service.SharedService;
-import org.joda.time.*;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.joda.time.DateTimeUtils;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static util.TestConstants.*;
 
 
-public class PopuladorDeDatosDeTest {
+public class PopuladorDeDatosDeTest extends Populador {
 
 	private static final String APPLICATION_CONTEXT = "classpath:spring/business-config.xml";
 	private static final String SECURITY_CONTEXT = "classpath:spring/security-config.xml";
 
-	private Map<String, Object> storedObjects = new HashMap<String, Object>();
-	private SharedService sharedService;
-	private PasswordEncoder passwordEncoder;
 
-	
+	private PopuladorDeDatosDeTest() {
+		super(APPLICATION_CONTEXT, SECURITY_CONTEXT);
+	}
+
 	public static void main(String[] args) throws ExcepcionDeReglaDelNegocio {
 		
 		DateTimeUtils.setCurrentMillisFixed(CURRENT_TIME_MOCK);
@@ -51,8 +37,6 @@ public class PopuladorDeDatosDeTest {
 	}
 
 	private void ejecutar() throws ExcepcionDeReglaDelNegocio {
-
-		inicializarDaos(APPLICATION_CONTEXT, SECURITY_CONTEXT);
 
 		popularCiudades();
 		popularBarrios();
@@ -73,13 +57,6 @@ public class PopuladorDeDatosDeTest {
 		sacarTurnos();
 	}
 
-	private void inicializarDaos(String... contexts) {
-
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(contexts);
-		sharedService = (SharedService) ctx.getBean("sharedServiceImpl");
-		passwordEncoder = (PasswordEncoder) ctx.getBean("passwordEncoder");
-	}
-
 	private void popularCiudades() {
 
 		popularCiudad("CIUDAD AUTONOMA DE BUENOS AIRES", null, null);
@@ -94,12 +71,15 @@ public class PopuladorDeDatosDeTest {
 
 	private void popularUsuarios() throws ExcepcionDeReglaDelNegocio {
 
-		popularUsuario("Victor Del Rio", "victor@gmail.com", "123456");
-		popularUsuario("Manuel Rodriguez","manuel@gmail.com", "123456");
-		popularUsuario("Juan Sanchez", "juan@gmail.com", "123456");
-		popularUsuario("Carlos Del Rio", "carlos@gmail.com", "123456");
-		popularUsuario("Admin Agenda1", "admin1@gmail.com", "123456");
-		popularUsuario("Admin Agenda2", "admin2@gmail.com", "123456");
+		Ciudad caba = (Ciudad) storedObjects.get("ciudad - CIUDAD AUTONOMA DE BUENOS AIRES");
+		Ciudad onceSept = (Ciudad) storedObjects.get("ciudad - 11 DE SEPTIEMBRE");
+
+		popularUsuario("Victor Del Rio", "victor@gmail.com", "123456", caba);
+		popularUsuario("Manuel Rodriguez","manuel@gmail.com", "123456", caba);
+		popularUsuario("Juan Sanchez", "juan@gmail.com", "123456", caba);
+		popularUsuario("Carlos Del Rio", "carlos@gmail.com", "123456", caba);
+		popularUsuario("Admin Agenda1", "admin1@gmail.com", "123456", onceSept);
+		popularUsuario("Admin Agenda2", "admin2@gmail.com", "123456", onceSept);
 	}
 
 	private void asignarRolesAdmin() throws ExcepcionDeReglaDelNegocio {
@@ -153,125 +133,5 @@ public class PopuladorDeDatosDeTest {
 		sacarTurno((Cliente)((Usuario)storedObjects.get("usuario - Victor Del Rio")).getRol(Cliente.NOMBRE_DE_ROL), (Agenda)storedObjects.get("agenda - Dra. Lopez"), INTERVALO_EN_JORNADA_HABITUAL);
 		sacarTurno((Cliente)((Usuario)storedObjects.get("usuario - Manuel Rodriguez")).getRol(Cliente.NOMBRE_DE_ROL), (Agenda)storedObjects.get("agenda - Dra. Lopez"), INTERVALO_EN_JORNADA_HABITUAL);
 	}
-
-	/*------------------------------------- Populadores ------------------------------------*/
-
-	private void popularCiudad(String nombre, String codigoPostal, String provincia) {
-
-		Ciudad ciudad = new Ciudad(nombre, codigoPostal, provincia);
-
-		sharedService.save(ciudad);
-		storedObjects.put("ciudad - " + nombre, ciudad);
-	}
-
-	private void popularBarrio(String nombre, Ciudad ciudad) {
-
-		Barrio barrio = new Barrio(nombre, ciudad);
-
-		sharedService.save(barrio);
-		storedObjects.put("barrio - " + nombre, barrio);
-	}
-
-	private void popularUsuario(String nombre, String email, String password) throws ExcepcionDeReglaDelNegocio {
-
-		Usuario usuario = new Usuario(nombre, email, passwordEncoder.encode(password));
-		usuario.agregarRol(new Cliente());
-
-		sharedService.save(usuario);
-		storedObjects.put("usuario - " + nombre, usuario);
-	}
-
-	private void asignarRolAdmin(String nombreDeUsuario) throws ExcepcionDeReglaDelNegocio {
-
-		Usuario usuario = (Usuario) storedObjects.get("usuario - " + nombreDeUsuario);
-		usuario.agregarRol(new AdministradorDeAgenda());
-
-		sharedService.update(usuario);
-	}
-
-	private void popularRubro(String nombre) {
-
-		Rubro rubro = new Rubro(nombre);
-
-		sharedService.save(rubro);
-		storedObjects.put("rubro - " + nombre, rubro);
-	}
-
-	private void popularPrestador(String nombre, Ubicacion ubicacion, String telefono, String imagen, Rubro rubro) throws ExcepcionDeReglaDelNegocio {
-
-		PrestadorDeServicios prestador = new PrestadorDeServicios(nombre, ubicacion, telefono, imagen);
-		rubro.agregarPrestadorDeServicios(prestador);
-
-		sharedService.save(prestador);
-		storedObjects.put("prestador - " + nombre, prestador);
-	}
-
-	private void popularAgenda(PrestadorDeServicios prestadorDeServicios, String nombre, Period antelacionMaxima, List<Usuario> administradores) throws ExcepcionDeReglaDelNegocio {
-
-		Agenda agenda = new Agenda(nombre, antelacionMaxima);
-		prestadorDeServicios.agregarAgenda(agenda);
-
-		sharedService.save(agenda);
-
-		for (Usuario usuario : administradores) {
-
-			((AdministradorDeAgenda) usuario.getRol(AdministradorDeAgenda.NOMBRE_DE_ROL)).getAgendas().add(agenda);
-			sharedService.update(usuario);
-		}
-
-		storedObjects.put("agenda - " + nombre, agenda);
-	}
-
-	private void popularServicio(PrestadorDeServicios prestadorDeServicios, String nombre, List<Agenda> agendas) {
-
-		Servicio servicio = new Servicio(nombre);
-
-		for (Agenda agenda : agendas)
-			servicio.getAgendas().add(agenda);
-
-		prestadorDeServicios.agregarServicio(servicio);
-
-		sharedService.save(servicio);
-		storedObjects.put("servicio - " + nombre, servicio);
-	}
-
-	private void popularJornadaHabitual(Agenda agenda, String nombre, DayOfWeekTime diaYHoraDeInicio, DayOfWeekTime diaYHoraDeFin,
-										Duration duracionDeLosTurnos, Integer vacantesPorTurno) throws ExcepcionDeReglaDelNegocio {
-
-		JornadaLaboralHabitual jornadaHabitual = new JornadaLaboralHabitual(diaYHoraDeInicio, diaYHoraDeFin, duracionDeLosTurnos, vacantesPorTurno);
-		agenda.agregarJornadaLaboralHabitual(jornadaHabitual);
-
-		sharedService.update(agenda);
-		storedObjects.put("jhabitual - " + nombre, jornadaHabitual);
-	}
-
-	private void popularJornadaOcasional(Agenda agenda, String nombre, DateTime desde, DateTime hasta,
-										 Duration duracionDeLosTurnos, Integer vacantesPorTurno) throws ExcepcionDeReglaDelNegocio {
-
-		JornadaLaboralOcasional jornadaOcasional = new JornadaLaboralOcasional(desde, hasta, duracionDeLosTurnos, vacantesPorTurno);
-		agenda.agregarJornadaLaboralOcasional(jornadaOcasional);
-
-		sharedService.update(agenda);
-		storedObjects.put("jocasional - " + nombre, jornadaOcasional);
-	}
-
-	private void popularDiaNoLaboral(Agenda agenda, String nombre, LocalDate fecha, String motivo) throws ExcepcionDeReglaDelNegocio {
-
-		DiaNoLaboral diaNoLaboral = new DiaNoLaboral(fecha, motivo);
-		agenda.agregarDiaNoLaboral(diaNoLaboral);
-
-		sharedService.save(diaNoLaboral);
-		storedObjects.put("dnl - " + nombre, diaNoLaboral);
-	}
-
-	private void sacarTurno(Cliente cliente, Agenda agenda, Interval interval) throws ExcepcionDeReglaDelNegocio {
-
-		Horario horarioASacarTurno = agenda.getHorario(interval);
-		horarioASacarTurno.sacarTurno(cliente);
-
-		sharedService.update(horarioASacarTurno);
-	}
-
-	/*--------------------------------------------------------------------------------------*/
 
 }
