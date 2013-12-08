3e79a67ce8d2b9d98db3b24dbbfb61b517042aeb
toto84toto <toto84toto@ca7791b7-9e57-4a50-a5bf-1408515eedb4>
2013-12-08 22:25:00 +0000

    Inicio ajax provincias

--- trunk/src/main/webapp/resources/application/js/register.js
+++ trunk/src/main/webapp/resources/application/js/register.js
@@ -9,6 +9,17 @@ $(document).ready(function() {
 	$("#provincia").chosen();
 	$("#ciudad").chosen();
 
+	$("#provincia").change(function() {
+
+		// TODO llamada ajax de las ciudades dada la provincia seleccionada
+
+		$.each(result, function() {
+			// TODO ver si hay una forma de mandar el toString para no concatenar strings
+			$("#ciudad").append($("<option />").val(this.id).text(this.nombre + " (" + this.codigoPostal + ")"));
+		});
+
+	});
+
 	$(".register-form").submit(function(event) {
 
 		var $form = $(this);
